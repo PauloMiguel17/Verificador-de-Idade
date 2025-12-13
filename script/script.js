94222974ecc8ab7bab76d52b6929d1dfdf87d5a3
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2025-12-13 16:37:50 -0300

    adição da interatividade e fotos

--- script/script.js
+++ script/script.js
@@ -9,6 +9,44 @@ function verificar(){
     }else{
         var fsex = document.getElementsByName('sexo')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `idade calculada ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = "Homem"
+            if(idade >= 0 && idade <= 10){
+                //criança
+                img.setAttribute('src', 'imagens/crianca_homem.png')
+            }else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem_homem.png')
+            }else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/homem_adulto.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'imagens/homem_idoso.png')
+            }
+
+        }
+        if (fsex[1].checked){
+            genero = "Mulher"
+            if(idade >= 0 && idade <= 10){
+                //criança
+                img.setAttribute('src', 'imagens/crianca_mulher.png')
+            }else if(idade < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovem_mulher.png')
+            }else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/mulher_adulta.png')
+            }else{
+                //idoso
+                img.setAttribute('src', 'imagens/mulher_idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
