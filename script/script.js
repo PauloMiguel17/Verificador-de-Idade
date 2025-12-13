cab92caef33db37f93eddbbfff4bd55255e1e8d6
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2025-12-13 16:14:59 -0300

    teste de função

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
 function verificar(){
-    window.alert('aaaaaaaaaaaaaaaaaaaaa')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('verifique os dados inseridos')
+    }else{
+        var fsex = document.getElementsByName('sexo')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `idade calculada ${idade}`
+    }
 }
