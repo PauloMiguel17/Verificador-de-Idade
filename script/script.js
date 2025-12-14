c31187d0598d94a765be4c5315e81da6e95986c2
PauloMiguel17 <paulomiguelpaes2@gmail.com>
2025-12-14 14:14:24 -0300

    introdução da função que altera a cor de fundo

--- script/script.js
+++ script/script.js
@@ -50,3 +50,10 @@ function verificar(){
         res.appendChild(img)
     }
 }
+
+function alterar(){
+    const cor = document.getElementById('icolor');
+    const corsel = cor.value;
+    document.body.style.background = corsel
+    
+}
